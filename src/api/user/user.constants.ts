1b16992bff41f7a4b2cd52d58fba7d81de563c05
Vladislav Pestsov <pestsov.js@gmail.com>
2022-06-14 19:16:22 +0300

    create user exception
    This exception return answer with concreticate error, for example: user not found

--- src/api/user/user.constants.ts
+++ src/api/user/user.constants.ts
@@ -1,2 +1,6 @@
 export const UPDATE_SUCCES_MESSAGE = "User has been updated success";
 export const DELETE_SUCCES_MESSAGE = "User has been deleted success";
+
+export const USER_NOT_FOUND_MESSAGE = (id: string) => {
+  return `User with id "${id}" not found`;
+};
--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,43 +1,71 @@
 import { Request, Response } from "express";
 import statusCode from "../../core/enum/statusCode.enum";
-import { DELETE_SUCCES_MESSAGE, UPDATE_SUCCES_MESSAGE } from "./user.constants";
+import UserException from "./user.exception";
 import UserResponse from "./user.response";
 import UserService from "./user.service";
 
+import {
+  DELETE_SUCCES_MESSAGE,
+  UPDATE_SUCCES_MESSAGE,
+  USER_NOT_FOUND_MESSAGE,
+} from "./user.constants";
+
 const userService = new UserService();
 const userResponse = new UserResponse();
+const userException = new UserException();
 
 export default class UserController {
   async createUser(req: Request, res: Response) {
     const user = await userService.createUser(req.body);
-    const data = await userResponse.createObj(user);
+    const data = userResponse.createObj(user);
 
     return res.status(statusCode.CREATED).json(data);
   }
 
   async getUsers(req: Request, res: Response) {
     const users = await userService.getUsers();
-    const data = await userResponse.createObjs(users);
+    const data = userResponse.createObjs(users);
 
     return res.status(statusCode.OK).json(data);
   }
 
   async getUser(req: Request, res: Response) {
-    const user = await userService.getUser(req.params.id);
-    const data = await userResponse.createObj(user);
+    const { id } = req.params;
 
+    const user = await userService.getUser(id);
+    if (!user) {
+      const data = userException.userNotFound(USER_NOT_FOUND_MESSAGE(id));
+      res.status(statusCode.NOT_FOUND).json(data);
+    }
+
+    const data = userResponse.createObj(user);
     return res.status(statusCode.OK).json(data);
   }
 
   async updateUser(req: Request, res: Response) {
-    const user = await userService.updateUser(req.params.id, req.body);
+    const { id } = req.params;
+    const { body } = req;
+
+    const user = await userService.updateUser(id, body);
+    if (!user) {
+      const data = userException.userNotFound(USER_NOT_FOUND_MESSAGE(id));
+      res.status(statusCode.NOT_FOUND).json(data);
+    }
+
     const data = userResponse.createModifyObj(user, UPDATE_SUCCES_MESSAGE);
 
     return res.status(statusCode.OK).json(data);
   }
 
   async deleteUser(req: Request, res: Response) {
-    const user = await userService.deleteUser(req.params.id);
+    const { id } = req.params;
+
+    const user = await userService.deleteUser(id);
+    if (!user) {
+      const data = userException.userNotFound(USER_NOT_FOUND_MESSAGE(id));
+      res.status(statusCode.NOT_FOUND).json(data);
+    }
+
     const data = userResponse.createModifyObj(user, DELETE_SUCCES_MESSAGE);
 
     return res.status(statusCode.OK).json(data);
