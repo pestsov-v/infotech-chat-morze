91b5190dafa5df01f2f5efba1f7baa4f52230639
Vladislav Pestsov <pestsov.js@gmail.com>
2022-06-15 14:56:55 +0300

    create router for auth module

---
+++ src/api/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+export default class AuthController {
+  async signup() {}
+  async login() {}
+  async logout() {}
+}
---
+++ src/api/auth/auth.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import AuthController from "./auth.controller";
+import authPath from "./auth.router.path";
+
+const authRouter = Router();
+const authContoller = new AuthController();
+
+authRouter.post(authPath.signup, authContoller.signup);
+authRouter.post(authPath.login, authContoller.login);
+authRouter.post(authPath.logout, authContoller.logout);
