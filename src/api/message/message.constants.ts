a8379bea3e8e1277327177ac0bfb345a8ae39953
Vladislav Pestsov <pestsov.js@gmail.com>
2022-06-22 09:47:02 +0300

    create remove message method

--- src/api/message/message.constants.ts
+++ src/api/message/message.constants.ts
@@ -1,5 +1,6 @@
 export const USER_NOT_AUTH_MESSAGE =
-  "ou are not authorized. Please login first";
+  "You are not authorized. Please login first";
 
 export const USER_DUBLICATE_MESSAGE = "You cannot send a message to yourself";
 export const MESSAGE_NOT_FOUND_MESSAGE = "This message does not exist";
+export const MESSAGE_DELETE_SUCCESS_MESSAGE = "Message remove success"
