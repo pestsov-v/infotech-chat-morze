80c48566d94a35d323bcb21e3acb573076f00c4d
Vladislav Pestsov <pestsov.js@gmail.com>
2022-06-25 18:01:41 +0300

    add the auth description of swagger doc

--- src/api/auth/auth.exception.ts
+++ src/api/auth/auth.exception.ts
@@ -1,6 +1,9 @@
 import status from "../../core/enum/status.enum";
 import {
+  DUBLICATE_USERNAME_MESSAGE,
   HAVE_NOT_RULES_MESSAGE,
+  INCORRECT_USER_MESSAGE,
+  MISS_REQUIRED_FIELDS_MESSAGE,
   TOKEN_NOT_EXISTS_MESSAGE,
   UNAUTHORIZED_MESSAGE,
 } from "./constants/auth.constants";
@@ -24,6 +27,27 @@ export default class AuthException {
     return {
       status: status.fail,
       message: TOKEN_NOT_EXISTS_MESSAGE,
+    };
+  }
+
+  dublicateUsername() {
+    return {
+      status: status.fail,
+      message: DUBLICATE_USERNAME_MESSAGE,
+    };
+  }
+
+  missRequiredFields() {
+    return {
+      status: status.fail,
+      message: MISS_REQUIRED_FIELDS_MESSAGE,
+    };
+  }
+
+  incorrectUser() {
+    return {
+      status: status.fail,
+      message: INCORRECT_USER_MESSAGE,
     };
   }
 }
